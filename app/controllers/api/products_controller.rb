3ddf09fb5e17cce49c31a2a7e78e2b51724f9e7b
mike <mikemcfaddin@gmail.com>
2020-01-17 17:51:56 -0500

    adds query and segment routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,24 @@
 class Api::ProductsController < ApplicationController
 
   def all_products_action
-    # render "product.json.rb"
+    @products = Product.all #array of hashes
     render "all_products.json.jb"
+  end
+
+  def first_product_action
+    @product = Product.first #hash of product data
+    render "first_product.json.jb"
+  end
+
+  def second_product_action
+    @product = Product.find_by(id: 2)
+    render "first_product.json.jb"
+  end
+
+  def any_product_action
+    product_id = params[:id] #params["id"]
+    @product = Product.find_by(id: product_id)
+    render "any_product.json.jb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products_url" => "products#all_products_action"
+
+    get "/first_product_url" => "products#first_product_action"
+
+    get "/second_product_url" => "products#second_product_action"
+
+    
+
+    get "/any_product_url" => "products#any_product_action"
+
+    get "/any_product_url/:id" => "products#any_product_action"
   end
 
 end
